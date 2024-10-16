f28d7a1b1258dddffe3578441e6df07e1fb9559e
Marcio junior <marciojunior5872@gmail.com>
2024-10-16 04:33:46 -0300

    finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,41 +1,41 @@
-import 'dotenv/config'
+import 'dotenv/config';
+import { existsSync, mkdirSync } from 'node:fs';
+import { resolve } from 'node:path';
+import fastify from 'fastify';
+import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
+import multipart from '@fastify/multipart';
+import { memoriesRoutes } from './routes/memories';
+import { authRoutes } from './routes/auth';
+import { uploadRoutes } from './routes/upload';
 
-import fastify from 'fastify'
-import cors from '@fastify/cors'
-import jwt from '@fastify/jwt'
-import multipart from '@fastify/multipart'
-import { memoriesRoutes } from './routes/memories'
-import { authRoutes } from './routes/auth'
-import { uploadRoutes } from './routes/upload'
-import { resolve } from 'node:path'
+// Crie a pasta 'uploads' se não existir
+const uploadDir = resolve(__dirname, '../uploads');
+if (!existsSync(uploadDir)) {
+  mkdirSync(uploadDir);
+}
 
-const app = fastify()
+const app = fastify();
 
-app.register(multipart)
-
+app.register(multipart);
 app.register(require('@fastify/static'), {
-  root: resolve(__dirname, '../uploads'),
+  root: uploadDir,
   prefix: '/uploads',
-})
-
+});
 app.register(cors, {
   origin: true,
-})
-
+});
 app.register(jwt, {
   secret: 'spacetime',
-})
+});
 
-app.register(authRoutes)
-app.register(uploadRoutes)
-app.register(memoriesRoutes)
+app.register(authRoutes);
+app.register(uploadRoutes);
+app.register(memoriesRoutes);
 
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log(`🚀 HTTP server running on port ${process.env.PORT || 3333}`)
-  })
+const port = process.env.PORT || 3333;
+app.listen({ port: Number(port), host: '0.0.0.0' }).then(() => {
+  console.log(`🚀 HTTP server running on port ${port}`);
+});
 
 module.exports = app;

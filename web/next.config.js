d57836386720d991a2d0ee21549ed783603c4ad8
Marcio junior <marciojunior5872@gmail.com>
2024-10-16 16:51:56 -0300

    ..

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', 'localhost', 'https://space-timelinee.vercel.app/'],
+    domains: ['avatars.githubusercontent.com', 'localhost', 'https://space-timeline.vercel.app/'],
   },
 }
 

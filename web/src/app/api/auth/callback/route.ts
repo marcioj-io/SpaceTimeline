702a23d9e3322878c142a900884f5720345877c3
Marcio junior <marciojunior5872@gmail.com>
2024-10-18 19:38:14 -0300

    ..

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -18,7 +18,7 @@ export async function GET(request: NextRequest) {
 
   return NextResponse.redirect(redirectURL, {
     headers: {
-      'Set-Cookie': `token=${token}; Path=/; HttpOnly; SameSite=None; max-age=${cookieExpiresInSeconds}`,
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds}`,
     },
   })
 }

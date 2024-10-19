fcb7e7129c69bdd3e4decd4e42b1b0be69147c80
Marcio junior <marciojunior5872@gmail.com>
2024-10-19 19:40:29 -0300

    ...

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,26 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: 'https://space-timeline-server.vercel.app/',
   // baseURL: 'http://localhost:3333/',
 })
-
-api.interceptors.request.use(
-  (config) => {
-    const token = document.cookie
-      .split('; ')
-      .find((row) => row.startsWith('tkk='))
-    const tokenValue = token ? token.split('=')[1] : null
-
-    if (tokenValue) {
-      config.headers.Authorization = `Bearer ${tokenValue}`
-    }
-
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  },
-)
-
-export { api }

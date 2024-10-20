370e44c6f0729681e645357d2c7221da725d9c73
Marcio junior <marciojunior5872@gmail.com>
2024-10-19 21:37:09 -0300

    ...

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,5 @@
 // src/app/page.tsx
-
-'use client'; // Isso transforma o componente em um componente de cliente
+// 'use client'
 
 import { EmptyMemories } from '@/components/EmptyMemories';
 import { api } from '@/lib/api';
@@ -9,7 +8,7 @@ import ptBR from 'dayjs/locale/pt-br';
 import Image from 'next/image';
 import Link from 'next/link';
 import { ArrowRight } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { cookies } from 'next/headers';
 
 dayjs.locale(ptBR);
 
@@ -20,58 +19,74 @@ interface Memory {
   createdAt: string;
 }
 
-export default function Home() {
-  const [memories, setMemories] = useState<Memory[]>([]);
+export default async function Home() {
+  // const [memories, setMemories] = useState<Memory[]>([]);
+  const token = cookies().get('tkk')?.value
 
-  const [token, setToken] = useState<string>();
-  const cookieToken = document.cookie.split('; ').find(row => row.startsWith('tkk='))?.split('=')[1];
-  setToken(cookieToken)
+  // const [token, setToken] = useState<string>();
+  // const cookieToken = document.cookie.split('; ').find(row => row.startsWith('tkk='))?.split('=')[1];
+  // setToken(cookieToken)
 
-  useEffect(() => {
-    const fetchMemories = async (token: string | undefined) => {
-      const response = await api.get('/memories', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setMemories(response.data || []);
-    };
+  // useEffect(() => {
+  //   const fetchMemories = async (token: string | undefined) => {
+  //     const response = await api.get('/memories', {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     });
+  //     setMemories(response.data || []);
+  //   };
 
-    fetchMemories(token)
+  //   fetchMemories(token)
 
-  }, [token]);
+  // }, [token]);
+  let memories = [];
 
-  if (memories.length === 0) {
-    return <EmptyMemories />;
+  try {
+    memories = await api.get('/memories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(t => t.data);
+    // setMemories(response.data || []);
+
+    if (memories.length === 0) {
+      return <EmptyMemories />;
+    }
+
+    return (
+      <div className="flex flex-col gap-10 p-8">
+        {memories.map((memory: Memory) => (
+          <div key={memory.id} className="space-y-4">
+            <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
+              {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
+            </time>
+            <Image
+              src={memory.coverUrl}
+              alt=""
+              width={592}
+              height={280}
+              quality={100}
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <p className="text-lg leading-relaxed text-gray-100">
+              {memory.excerpt}
+            </p>
+            <Link
+              href={`/memories/${memory.id}`}
+              className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
+            >
+              Ler mais
+              <ArrowRight className="h-4 w-4" />
+            </Link>
+          </div>
+        ))}
+      </div>
+    );
+  } catch (erro) {
+    return {
+      'error': "Error message",
+      'status': 500
+    }
   }
-
-  return (
-    <div className="flex flex-col gap-10 p-8">
-      {memories.map((memory) => (
-        <div key={memory.id} className="space-y-4">
-          <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
-            {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
-          </time>
-          <Image
-            src={memory.coverUrl}
-            alt=""
-            width={592}
-            height={280}
-            quality={100}
-            className="aspect-video w-full rounded-lg object-cover"
-          />
-          <p className="text-lg leading-relaxed text-gray-100">
-            {memory.excerpt}
-          </p>
-          <Link
-            href={`/memories/${memory.id}`}
-            className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
-          >
-            Ler mais
-            <ArrowRight className="h-4 w-4" />
-          </Link>
-        </div>
-      ))}
-    </div>
-  );
 }
